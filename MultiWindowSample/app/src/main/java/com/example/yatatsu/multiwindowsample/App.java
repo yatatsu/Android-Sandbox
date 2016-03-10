d7958599d37d9c09a75ea5b37a7e6f68c5528a6b
yatatsu <yatatsukitagawa@gmail.com>
2016-03-11 01:03:59 +0900

    Initial commit.

---
+++ MultiWindowSample/app/src/main/java/com/example/yatatsu/multiwindowsample/App.java
@@ -0,0 +1,14 @@
+package com.example.yatatsu.multiwindowsample;
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Timber.plant(new Timber.DebugTree());
+    }
+}
